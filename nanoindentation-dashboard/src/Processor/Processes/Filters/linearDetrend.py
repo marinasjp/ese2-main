333b03eb332fb951f3dab9f26efb811420ac8f0f
2569221s <2569221s@student.gla.ac.uk>
2023-02-01 16:04:41 +0000

    Fix linearDetrend

--- nanoindentation-dashboard/src/Processor/Processes/Filters/linearDetrend.py
+++ nanoindentation-dashboard/src/Processor/Processes/Filters/linearDetrend.py
@@ -1,11 +1,14 @@
 import numpy as np
 from scipy.signal import savgol_filter
 
+window = 101  # INPUT FROM USER
+threshold = 10
 
-def calculate(self, x, y):
-  dy = savgol_filter(y, self.getValue('window'), 3, deriv=1)
+
+def calculate(x, y):
+  dy = savgol_filter(y, window, 3, deriv=1)
   for j in range(len(dy)):
-    if dy[j] > self.getValue('threshold') / 1e12:
+    if dy[j] > threshold / 1e12:
       break
   for k in range(j, 0, -1):
     if dy[k] < 0:
