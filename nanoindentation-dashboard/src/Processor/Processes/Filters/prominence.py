f8edea0adaded6ef4f4b46913650a0105141e584
2569221s <2569221s@student.gla.ac.uk>
2023-02-01 16:07:33 +0000

    Implement prominence filter

---
+++ nanoindentation-dashboard/src/Processor/Processes/Filters/prominence.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.signal import find_peaks
+from scipy.interpolate import interp1d
+
+# USER INPUT
+band = 30
+pro = 40
+threshold = 25
+
+
+def calculate(x, y):
+  ff = np.fft.rfft(y, norm=None)
+  idex = find_peaks(np.log(np.abs(ff)), prominence=pro)
+  xgood = np.ones(len(ff.real)) > 0.5
+  for imax in idex[0]:
+    jwin = int(imax * band / 100)
+    if imax > threshold and jwin == 0:
+      xgood[imax] = False
+    elif imax > threshold:
+      ext1 = np.max([imax - jwin, 0])
+      ext2 = np.min([imax + jwin + 1, len(xgood) - 1])
+      for ii in range(ext1, ext2):
+        xgood[ii] = False
+  if np.sum(xgood) < 50:
+    return
+  xf = np.arange(0, len(ff.real))
+  yinterpreal = interp1d(xf[xgood], ff.real[xgood], kind='linear')
+  yinterpimag = interp1d(xf[xgood], ff.imag[xgood], kind='linear')
+  ff.real = yinterpreal(xf)
+  ff.imag = yinterpimag(xf)
+  return x, np.fft.irfft(ff, n=len(y))
