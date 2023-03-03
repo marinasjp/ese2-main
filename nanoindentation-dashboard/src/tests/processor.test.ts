a7690678790a803abcf9add874b9f98b9aa9fa30
“Alexandra <2550454m@student.gla.ac.uk>
2023-03-03 17:01:03 +0000

    Utilising jester for process.test

--- nanoindentation-dashboard/src/tests/processor.test.ts
+++ nanoindentation-dashboard/src/tests/processor.test.ts
@@ -21,25 +21,22 @@ let startPro = {
             }
 let testData: { x: number[], y: number[] } = { x: [100, 200, 300, 400], y: [1, 2, 3, 4] };
 let outData: { x: number[], y: number[] } = { x: [100, 200, 300, 400], y: [2, 3, 4, 5] };
+let service: ProcessorService;
 
-export class processorTest {
-    constructor(private processorService: ProcessorService) {}
-
+ describe('ProcessorService', () => {
+   
+    // Tests if the newProcess function is ... a new python script (Process) is added to the data structure.
+    test('should return 1', () => { // maybe test if it is at the end?
+        expect(service.addProcess(testProcess)).toBe(startPro);
+    });
     
-        describe(){
-        // Tests if the newProcess function is ... a new python script (Process) is added to the data structure.
-        test('should return 1', () => { // maybe test if it is at the end?
-            expect(this.processorService.addProcess(testProcess)).toBe(startPro);
-        });
+    // Tests if getScript ... is returning the correct script when given a Process name and type.
+    test('TesterProc should return func that adds 1 to dataset', () => {
+        expect(service.getScript(testProcess)).toBe(testStr);
+     });
     
-        // Tests if getScript ... is returning the correct script when given a Process name and type.
-        test('TesterProc should return func that adds 1 to dataset', () => {
-            expect(this.processorService.getScript(testProcess)).toBe(testStr);
-        });
-    
-        // Tests if doProces... is running on the given data set.
-        test('testing do process should return dataset with +1', () => {
-            expect(this.processorService.doProcess(testProcess, testData)).toBe(outData);
-        });
-        };
-}
+    // Tests if doProces... is running on the given data set.
+    test('testing do process should return dataset with +1', () => {
+        expect(service.doProcess(testProcess, testData)).toBe(outData);
+    });
+});
