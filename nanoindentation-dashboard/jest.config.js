f50212db1ccf61054673bf2e3e872ab3b965441a
Marina San Jose Pena <2569221s@student.gla.ac.uk>
2023-03-04 20:52:30 +0000

    Add replace command

--- nanoindentation-dashboard/jest.config.js
+++ nanoindentation-dashboard/jest.config.js
@@ -4,6 +4,11 @@
 //     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
 //     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 //   };
+import '@testing-library/jest-dom';
+//@ts-ignore
+import replaceAllInserter from 'string.prototype.replaceall';
+
+replaceAllInserter.shim();
 
 
 module.exports = {
