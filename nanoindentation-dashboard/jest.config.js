0dff618485008bc79a3a32e8c4f545978617fe1d
Marina San Jose Pena <2569221s@student.gla.ac.uk>
2023-03-04 21:32:51 +0000

    Change directory

--- nanoindentation-dashboard/jest.config.js
+++ nanoindentation-dashboard/jest.config.js
@@ -6,7 +6,11 @@
 //   };
 
 module.exports = {
-    "setupFilesAfterEnv": ["./jestSetup.ts"],
+    transform: {'^.+\\.ts?$': 'ts-jest'},
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    "setupFilesAfterEnv": ["<rootDir>/nanoindentation-dashboard/src/jestSetup.ts"],
   // "collectCoverageFrom": ["src/**/*.js", "!**/node_modules/**"],
   // "coverageReporters": ["html", "text", "text-summary", "cobertura"],
   "testMatch": [['<rootDir>/nanoindentation-dashboard/src/tests/**']]
