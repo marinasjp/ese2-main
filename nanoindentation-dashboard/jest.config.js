e107d5bb42506f010da9d97b792914222b5506b1
Marina San Jose Pena <2569221s@student.gla.ac.uk>
2023-03-04 21:40:08 +0000

    Fix path

--- nanoindentation-dashboard/jest.config.js
+++ nanoindentation-dashboard/jest.config.js
@@ -10,8 +10,8 @@ module.exports = {
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    "setupFilesAfterEnv": ["<rootDir>/nanoindentation-dashboard/src/jestSetup.ts"],
+    "setupFilesAfterEnv": ["src/jestSetup.ts"],
   // "collectCoverageFrom": ["src/**/*.js", "!**/node_modules/**"],
   // "coverageReporters": ["html", "text", "text-summary", "cobertura"],
-  "testMatch": [['<rootDir>/nanoindentation-dashboard/src/tests/**']]
+  "testMatch": [['<rootDir>/src/tests/**']]
 }
