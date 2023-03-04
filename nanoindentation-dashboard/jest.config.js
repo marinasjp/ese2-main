817f8c910d03a4467a68afe6109d84d5399cfca7
Marina San Jose Pena <2569221s@student.gla.ac.uk>
2023-03-04 22:38:21 +0000

    Change setup

--- nanoindentation-dashboard/jest.config.js
+++ nanoindentation-dashboard/jest.config.js
@@ -1,17 +1,17 @@
-// module.exports = {
-//     transform: {'^.+\\.ts?$': 'ts-jest'},
-//     testEnvironment: 'node',
-//     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-//   };
-
 module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    "setupFilesAfterEnv": ["<rootDir>/jestSetup.ts"],
-  // "collectCoverageFrom": ["src/**/*.js", "!**/node_modules/**"],
-  // "coverageReporters": ["html", "text", "text-summary", "cobertura"],
-  "testMatch": [['<rootDir>/src/tests/**']]
-}
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  };
+
+// module.exports = {
+//   transform: {'^.+\\.ts?$': 'ts-jest'},
+//   testEnvironment: 'node',
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+//   setupFilesAfterEnv: ["<rootDir>/jestSetup.ts"],
+//   // "collectCoverageFrom": ["src/**/*.js", "!**/node_modules/**"],
+//   // "coverageReporters": ["html", "text", "text-summary", "cobertura"],
+//   // testMatch: [['<rootDir>/src/tests/**']]
+//   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+// }
