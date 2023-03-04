ef1dee95e08cbf8a4f992781f6858092e3c99bb8
Marina San Jose Pena <2569221s@student.gla.ac.uk>
2023-03-04 18:57:16 +0000

    Change config for CI

--- nanoindentation-dashboard/jest.config.js
+++ nanoindentation-dashboard/jest.config.js
@@ -1,6 +1,13 @@
+// module.exports = {
+//     transform: {'^.+\\.ts?$': 'ts-jest'},
+//     testEnvironment: 'node',
+//     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+//   };
+
+
 module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-  };
+  "collectCoverageFrom": ["src/**/*.js", "!**/node_modules/**"],
+  "coverageReporters": ["html", "text", "text-summary", "cobertura"],
+  "testMatch": ["**/*.test.js"]
+}
--- nanoindentation-dashboard/src/tests/processor.test.ts
+++ nanoindentation-dashboard/src/tests/processor.test.ts
@@ -24,17 +24,17 @@ let outData: { x: number[], y: number[] } = { x: [100, 200, 300, 400], y: [2, 3,
 let service: ProcessorService;
 
  describe('ProcessorService', () => {
-   
+
     // Tests if the newProcess function is ... a new python script (Process) is added to the data structure.
     test('should return 1', () => { // maybe test if it is at the end?
         expect(service.addProcess(testProcess)).toBe(startPro);
     });
-    
+
     // Tests if getScript ... is returning the correct script when given a Process name and type.
     test('TesterProc should return func that adds 1 to dataset', () => {
         expect(service.getScript(testProcess)).toBe(testStr);
      });
-    
+
     // Tests if doProces... is running on the given data set.
     test('testing do process should return dataset with +1', () => {
         expect(service.doProcess(testProcess, testData)).toBe(outData);
