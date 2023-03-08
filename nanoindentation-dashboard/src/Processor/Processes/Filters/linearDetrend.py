b6e2718ace09672a0f4f6792ec7635bfc046f46c
2569221s <2569221s@student.gla.ac.uk>
2023-03-08 12:19:15 +0000

    Fix linearDetrend. Explained in issue #126

--- nanoindentation-dashboard/src/Processor/Processes/Filters/linearDetrend.py
+++ nanoindentation-dashboard/src/Processor/Processes/Filters/linearDetrend.py
@@ -1,22 +1,27 @@
 import numpy as np
-from scipy.signal import savgol_filter
+import scipy
 
-window = 101  # INPUT FROM USER
-threshold = 10
+# THIS IS BROKEN CUSTOMER CODE
+# window = 101  # INPUT FROM USER
+# threshold = 10
+#
+#
+# def calculate(x, y):
+#   x_base, y_base = get_baseline(x, y)
+#   m, q = np.polyfit(x_base, y_base, 1)
+#   return x, y - m * x - q
+#
+#
+# def get_baseline(x, y):
+#   dy = savgol_filter(y, window, 3, deriv=1)
+#   for j in range(len(dy)):
+#     if dy[j] > threshold / 1e12:
+#       break
+#   for k in range(j, 0, -1):
+#     if dy[k] < 0:
+#       break
+#   return [x[:k], y[:k]]
 
-
-def calculate(x, y):
-  x_base, y_base = get_baseline(x, y)
-  m, q = np.polyfit(x_base, y_base, 1)
-  return x, y - m * x - q
-
-
-def get_baseline(x, y):
-  dy = savgol_filter(y, window, 3, deriv=1)
-  for j in range(len(dy)):
-    if dy[j] > threshold / 1e12:
-      break
-  for k in range(j, 0, -1):
-    if dy[k] < 0:
-      break
-  return [x[:k], y[:k]]
+def calculate(x):
+  detrendedData = scipy.signal.detrend(data=x, axis=-1, type='linear', bp=0, overwrite_data=False)
+  return detrendedData
