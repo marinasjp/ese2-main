79add994a7afee80efa4c9cd1cd98fe34061e9a7
2569221s <2569221s@student.gla.ac.uk>
2023-03-08 17:13:50 +0000

    Make test for rov #137, #139

--- nanoindentation-dashboard/src/Processor/Processes/CPoint/test_rov.py
+++ nanoindentation-dashboard/src/Processor/Processes/CPoint/test_rov.py
@@ -1,6 +1,14 @@
 from rov import calculate
+import numpy as np
+
+
 def test_calculate():
-  # testx = [] we need test data from the customer to test
-  # testy = []
-  # calculate(testx, testy)
-  assert True
+  test_array = []
+  test_array2 = []
+  f = open("data.csv", "r")
+  for x in f:
+    xArr = x.split(",")
+    test_array.append(float(xArr[1]))
+    test_array2.append(float(xArr[4]))
+  f.close()
+  assert calculate(np.array(test_array),np.array(test_array2)) == [-0.007434, 4.354564]
