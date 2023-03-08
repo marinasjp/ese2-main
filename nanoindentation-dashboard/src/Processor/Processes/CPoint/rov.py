7c3fb6b07c318ad4e88321edab3d6e3a838effe2
2569221s <2569221s@student.gla.ac.uk>
2023-03-08 17:15:34 +0000

    include user input in rov #126,#147

--- nanoindentation-dashboard/src/Processor/Processes/CPoint/rov.py
+++ nanoindentation-dashboard/src/Processor/Processes/CPoint/rov.py
@@ -1,19 +1,16 @@
 import numpy as np
 
-#User Inputs
-Fthreshold = 10
-windowr = 200
-Xrange = 1000
 
-
-def calculate( x, y):
+def calculate(x, y):
   zz_x, rov = getWeight(x, y)
+  print(zz_x)
+  print(rov)
   rov_best_ind = np.argmax(rov)
   j_rov = np.argmin((x - zz_x[rov_best_ind]) ** 2)
   return [x[j_rov], y[j_rov]]
 
 
-def getRange(x, y):
+def getRange(x, y, Fthreshold = 10, Xrange = 1000):
   try:
     jmax = np.argmin((y - Fthreshold * 1e-9) ** 2)
     jmin = np.argmin((x - (x[jmax] - Xrange * 1e-9)) ** 2)
@@ -22,7 +19,7 @@ def getRange(x, y):
   return jmin, jmax
 
 
-def getWeight(x, y):
+def getWeight(x, y, windowr = 200):
   jmin, jmax = getRange(x, y)
   winr = windowr * 1e-9
   xstep = (max(x) - min(x)) / (len(x) - 1)
@@ -36,3 +33,9 @@ def getWeight(x, y):
     rov.append((np.var(y[j + 1: j + win])) / (np.var(y[j - win: j - 1])))
   return x[jmin:jmax], rov
 
+
+# test_array = [4, 5, 7, 2, 23, 6, 7, 32, 4, 5, 7, 2, 23, 6, 7, 32, 4, 5, 7, 2, 23, 6, 7, 324, 5, 7, 2, 23, 6, 7, 324,
+#                 2, 23, 6, 7, 324, 5, 7, 2, 23, 6, 7, 324, 5, 7, 2, 23, 6, 7, 324, 5, 7, 2, 23, 6, 7, 324, 5, 7, 2, 23,
+#                 324, 5, 7, 2, 23, 6, 7, 324, 5, 7, 2, 23, 6, 7, 324, 5, 7, 2, 23, 6, 7, 324, 5, 7, 2, 23, 6, 7, 324, 5,
+#                 23, 6, 7, 32]
+# calculate(np.array(test_array), np.array([1, 2, 1e-09, 3, 1e-08, 1]))
