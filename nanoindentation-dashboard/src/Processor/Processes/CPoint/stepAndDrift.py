a01ea5b67e11195bf6629d37b7ed22d06da803c1
2569221s <2569221s@student.gla.ac.uk>
2023-03-08 16:16:05 +0000

    Fix stepAndDrift to be able to run #126, #146

--- nanoindentation-dashboard/src/Processor/Processes/CPoint/stepAndDrift.py
+++ nanoindentation-dashboard/src/Processor/Processes/CPoint/stepAndDrift.py
@@ -1,16 +1,13 @@
 from scipy.signal import savgol_filter
-#User Inputs
-window = 101
-threshold = 10
-thratio = 25
 
-def calculate(x, y):
+
+def calculate(x, y, window = 101, threshold = 10, thratio = 25):
   dy = savgol_filter(y,window, 3, deriv=1)
   for j in range(len(dy)):
     if dy[j] > threshold / 1e12:
       break
+  k=j
   for k in range(j, 0, -1):
     if dy[k] < thratio * threshold / 1e14:
       break
   return [x[k], y[k]]
-
