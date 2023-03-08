e937b0a101b2c64cdd079959b06867d42039609c
“Alexandra <2550454m@student.gla.ac.uk>
2023-03-08 12:18:27 +0000

    Objects declared in unit test file

--- nanoindentation-dashboard/src/tests/processor.test.ts
+++ nanoindentation-dashboard/src/tests/processor.test.ts
@@ -1,7 +1,10 @@
 import { EProcType } from '../app/models/process.model';
-import { GraphService } from 'src/app/services/graph.service';
+import { GraphService } from '../app/services/graph.service';
 import { ProcessorService } from '../app/services/processor.service';
 import * as fs from 'fs';
+import { ErrorHandlerService } from '../app/services/error-handler.service';
+import { SampleDataService } from '../app/services/sample-data.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 let testProcess = {id:"test", name: "Test", procType: EProcType.TEST}
 //let testStr = fs.readFileSync('../assets/Processes/Tests/testProcess.py', 'utf-8');
@@ -21,22 +24,33 @@ let startPro = {
             }
 let testData: { x: number[], y: number[] } = { x: [100, 200, 300, 400], y: [1, 2, 3, 4] };
 let outData: { x: number[], y: number[] } = { x: [100, 200, 300, 400], y: [2, 3, 4, 5] };
-let service: ProcessorService;
+
+//Mock the Processor object to use it in testing
 
  describe('ProcessorService', () => {
 
+    let mockProcessorService: ProcessorService;
+    let errorHandler = new ErrorHandlerService();
+    let sample = new SampleDataService();
+    let http: HttpClient;
+    let graph = new GraphService(sample, http);
+
+    beforeEach(() => {
+        mockProcessorService = new ProcessorService(http, errorHandler, graph);
+    });
+
     // Tests if the newProcess function is ... a new python script (Process) is added to the data structure.
     test('should return 1', () => { // maybe test if it is at the end?
-        expect(service.addProcess(testProcess)).toBe(startPro);
+        expect(mockProcessorService.addProcess(testProcess)).toBe(startPro);
     });
 
     // Tests if getScript ... is returning the correct script when given a Process name and type.
     test('TesterProc should return func that adds 1 to dataset', () => {
-        expect(service.getScript(testProcess)).toBe(testStr);
+        expect(mockProcessorService.getScript(testProcess)).toBe(testStr);
      });
 
     // Tests if doProces... is running on the given data set.
     test('testing do process should return dataset with +1', () => {
-        expect(service.doProcess(testProcess, testData)).toBe(outData);
+        expect(mockProcessorService.doProcess(testProcess, testData)).toBe(outData);
     });
 });
