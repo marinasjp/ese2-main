ad895ceed37d04d5f3a5f2f95a736e7ec6e7fd41
2569221s <2569221s@student.gla.ac.uk>
2023-03-08 17:52:44 +0000

    Create test for rov #137

--- nanoindentation-dashboard/src/Processor/Processes/CPoint/test_rov.py
+++ nanoindentation-dashboard/src/Processor/Processes/CPoint/test_rov.py
@@ -11,4 +11,6 @@ def test_calculate():
     test_array.append(float(xArr[1]))
     test_array2.append(float(xArr[4]))
   f.close()
+  print(test_array)
+  print(test_array2)
   assert calculate(np.array(test_array),np.array(test_array2)) == [-0.007434, 4.354564]
