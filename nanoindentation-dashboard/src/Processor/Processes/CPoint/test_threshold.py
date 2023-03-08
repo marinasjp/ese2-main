9a80fac8ca381f22c9b63ca2b4c79d8fd7298b39
2569221s <2569221s@student.gla.ac.uk>
2023-03-08 15:57:05 +0000

    Make test for threshold. Issue #137 task #138

--- nanoindentation-dashboard/src/Processor/Processes/CPoint/test_threshold.py
+++ nanoindentation-dashboard/src/Processor/Processes/CPoint/test_threshold.py
@@ -1,5 +1,12 @@
+from threshold import calculate
+import numpy as np
+
 def test_calculate():
   # testx = [] we need test data from the customer to test
   # testy = []
-  # calculate(testx, testy)
-  assert True
+  # y needs to be smaller than x
+  assert calculate([4, 5, 7, 2, 23, 6, 7, 32], [1, 2, 3, 4, 3, 2, 1]) == False
+
+  assert calculate([4, 5, 7, 2, 23, 6, 7, 32], [1, 2, 3, 4, 3, 1e-08, 1]) == False
+
+  assert calculate(np.array([4, 5, 7, 2, 23, 6, 7, 32]), np.array([1, 2, 1e-09,3, 1e-08, 1])) == [2.0,3.0]
