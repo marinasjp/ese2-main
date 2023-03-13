ad251fc7b3b031dc0b84ec13330bc7a9b09bd498
“Alexandra <2550454m@student.gla.ac.uk>
2023-03-13 17:13:38 +0000

    doProcess became a private method, momentarilly removed test for it

--- nanoindentation-dashboard/src/app/services/processor.service.spec.ts
+++ nanoindentation-dashboard/src/app/services/processor.service.spec.ts
@@ -74,20 +74,20 @@ let startPro = {
 
 
     // Tests if the newProcess function is ... a new python script (Process) is added to the data structure.
-    it('should return 1', () => { // maybe test if it is at the end?
+    it('should return dataset with a new Process added', () => { // maybe test if it is at the end?
       const service: ProcessorService = TestBed.get(ProcessorService);
       expect(service.addProcess(testProcess)).toEqual(startPro);
     });
 
     // Tests if getScript ... is returning the correct script when given a Process name and type.
-    it('TesterProc should return func that adds 1 to dataset', () => {
+    it('Tests if getScript ... is returning the correct script when given a Process name and type.', () => {
       const service: ProcessorService = TestBed.get(ProcessorService);
       expect(service.getScript(testProcess)).toEqual(testStr);
      });
 
     // Tests if doProces... is running on the given data set.
-    it('testing do process should return dataset with +1', () => {
+   /* it('Tests if doProces... is running on the given data set', () => {
       const service: ProcessorService = TestBed.get(ProcessorService);
       expect(service.doProcess(testProcess, testData)).toEqual(outData);
-    });
+    }); */
 });
