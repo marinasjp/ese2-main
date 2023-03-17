4c376632fff529dbbb94bc0c68583ac91c2fedd8
“Alexandra <2550454m@student.gla.ac.uk>
2023-03-17 19:49:33 +0000

    Closes #156. Testing Suite is done

--- nanoindentation-dashboard/src/app/services/processor.service.spec.ts
+++ nanoindentation-dashboard/src/app/services/processor.service.spec.ts
@@ -12,24 +12,22 @@ import {WrittenProcesses} from "../../environments/environment";
 import { Dataset } from '../models/dataset.model';
 import { CustomError } from '../models/error.model';
 import { Datapoint } from '../models/datapoint.model';
-import {loadPyodide} from "pyodide";
 
-//import * as fs from 'fs';
-
+// Custom process to be used for testing
 let testProcess = {id:"testProcess", name: "Test", procType: EProcType.TEST, script:"def calculate( x, y): \n return x, y+1"}
-//let testFs = fs.readFileSync('../assets/Processes/Tests/testProcess.py', 'utf-8');
+// Custom python script to be used for testing, part of testProcess
 let testScript = "def calculate( x, y): \n return x, y+1"
 
+//Custom data set to be used for testing
 let testData: { x: number[], y: number[] } = { x: [100, 200, 300, 400], y: [1, 2, 3, 4] };
-let outData: { x: number[], y: number[] } = { x: [100, 200, 300, 400], y: [2, 3, 4, 5] };
 
-let modelProcessDataSet = WrittenProcesses;
-modelProcessDataSet.test.push(testProcess);
+let modelProcessDataSet = WrittenProcesses; // Dataset of recognised Processes taken from environment
+modelProcessDataSet.test.push(testProcess); //Push testProcess in it, to create custom WrittenProcesses for testing
 
 
-  describe('ProcessorService', () => {
-
+  describe('ProcessorService', () => { 
    
+    // Initialise all services & components needed for declaring ProcessorService
     let errorHandler = new ErrorHandlerService();
     let sample = new SampleDataService();
     let http: HttpClient;
@@ -38,6 +36,7 @@ modelProcessDataSet.test.push(testProcess);
     let process: ProcessorService;
 
     beforeEach(async() => {
+      //configure all imports and providers needed for testing
         
        TestBed.configureTestingModule({
         imports:[
@@ -55,63 +54,45 @@ modelProcessDataSet.test.push(testProcess);
 
 
     // Tests if addProcess is ... adding a new python script (Process) to the data structure.
-    it('Tests if addProcess ... returns dataset with a new Process added', () => { // maybe test if it is at the end?
-      const service: ProcessorService = TestBed.get(ProcessorService);
+    it('Tests if addProcess ... returns dataset with a new Process added', () => {
+      const service: ProcessorService = TestBed.get(ProcessorService); //declare mock ProcessorService
       expect(service.addProcess(testProcess)).toEqual(modelProcessDataSet);
     });
 
     // Tests if getScript ... is returning the correct script when given a Process name and type.
-    it('Tests if getScript ... is returning the correct script when given a Process name and type.', (done)=> {
+    it('Tests if getScript ... is returning the correct script when given a Process name and type.', (done)=> { //since there is a promise tested, this is not async.
     
-      const service: ProcessorService = TestBed.get(ProcessorService);
+      const service: ProcessorService = TestBed.get(ProcessorService); //declare mock ProcessorService
       let promise = service.getScript(testProcess);
 
-      if (promise instanceof Promise<String>){
-        
+      if (promise instanceof Promise<String>){ //ensure promise is an instance of the desired data type
         promise.then((result) =>{
           expect(result).toEqual(testScript);
-          done();
+          done(); //testing is done here
         })
       }
      });
 
-
-     //Test if getScript ... can grab process from the file system
-     it('Test if getScript ... can grab process from the file system.', () => {
-      const service: ProcessorService = TestBed.get(ProcessorService);
-      //figure out how to use fs from angular
-     });
-
      //Test if ... Pyodide, NumPy and SciPy have been loaded
      it('Test if ... Pyodide, NumPy and SciPy have been loaded', () => {
-      const spy = spyOn(ProcessorService.prototype, 'initPy').and.stub();
-      const service = new ProcessorService(http, errorHandler, graph);
+      const spy = spyOn(ProcessorService.prototype, 'initPy').and.stub(); //spy on initPy() inside constructor
+      const service = new ProcessorService(http, errorHandler, graph); //create actual instance to call the constructor
 
       expect(spy).toHaveBeenCalledTimes(1);
      }); 
 
      // Test if script executes
-     it('Test if ... correct script is selected to run', (done) => {
-      let service = new ProcessorService(http, errorHandler, graph); //declare new object because the constructor needs to run
+     it('Test if ... correct script is selected to run', () => {
+      let service = new ProcessorService(http, errorHandler, graph); //create instance because the constructor needs to run
 
-      service.addProcess(testProcess);
-      service.selectedTests = [testProcess];
+      service.addProcess(testProcess); //add process to the recognised processes
+      service.selectedTests = [testProcess]; //select process
 
       expect(service.selectedTests).toEqual([testProcess]);
-      done()
      });
 
-     it('Tests if ... pyodide runner is used', ()=>{
 
-     })
-
-
-     /*it('Test if ... Selected process runs on dataset',async()=>{
-      let service = new ProcessorService(http, errorHandler, graph);
-       let pyodide = await loadPyodide({
-      indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
-    });
-
+     it('Tests if runFrom ... results in runAll method used', ()=>{
       let testDataSet: Dataset = {
         contactPoint: null,
         displacementForceData : [],
@@ -121,17 +102,47 @@ modelProcessDataSet.test.push(testProcess);
         testData: [],
       };
 
+      const spy = spyOn<any>(ProcessorService.prototype, 'runAll').and.stub(); //spy on runAll() function
+      let service = new ProcessorService(http, errorHandler, graph); //create instance because the constructor needs to run
+
       let testDataConverted: any = service.convertXAndYArrayToDatapointsArray(testData);
       if (typeof (testDataConverted) == 'object' && testDataConverted.error)
       {
-        fail('Data conversion failed.');
+        fail('Data conversion failed.');                            //create needed Datasets to run the public method runFrom()
       }
-      testDataSet.testData = testDataConverted;
-      let outDataConverted: any = service.convertXAndYArrayToDatapointsArray(outData);
-      service.addProcess(testProcess);
-      service.selectedTests = [testProcess];
 
-      expect(service.testOutDatasets[0].testData).toEqual(outDataConverted);
-     });*/
+      testDataSet.testData = testDataConverted;
+      service.addProcess(testProcess);
+      service.runFrom(EProcType.TEST, testDataConverted); //call runFrom() method with configured Datasets
+
+      expect(spy).toHaveBeenCalledTimes(1); //expect runAll to have run once
+     });
+
+
+     it('Tests if ... data is successfully converted from X,Y to Datapoint[]', ()=>{
+
+      const service: ProcessorService = TestBed.get(ProcessorService);
+
+      let data1: Datapoint = {x:100, y:1};
+      let data2: Datapoint = {x:200, y:2};
+      let data3: Datapoint = {x:300, y:3};
+      let data4: Datapoint = {x:400, y:4};
+      let dataArray = [data1, data2, data3, data4] //create desired form of Datapoint[] model
+
+      expect(service.convertXAndYArrayToDatapointsArray(testData)).toEqual(dataArray);
+     });
+
+     it('Tests if ... data is successfully converted from Datapoint[] to X,Y', ()=>{
+
+      const service: ProcessorService = TestBed.get(ProcessorService);
+
+      let data1: Datapoint = {x:100, y:1};
+      let data2: Datapoint = {x:200, y:2};
+      let data3: Datapoint = {x:300, y:3};
+      let data4: Datapoint = {x:400, y:4};
+      let dataArray = [data1, data2, data3, data4] //create desired form of Datapoint[] model
+
+      expect(service.convertDatapointsArrayToXAndYArray(dataArray)).toEqual(testData);
+     });
     
 });
